b5cad2fe8973f82e005ec5cce67e30af77eb6dc6
surveybuilder-admin <64079569+surveybuilder-admin@users.noreply.github.com>
2023-04-07 08:29:54 -0400

    More stuff

--- SSQL/index.php
+++ SSQL/index.php
@@ -4,21 +4,30 @@ require_once('lib/ssql.lib.php');
 <html>
 <head>
 <title>SSQL</title>
+<?php
+$ssql = new SSQL();
+echo $ssql->style();
+?>
 </head>
 <body>
 <?php
-$ssql = new SSQL();
 if($ssql->setCredential('localhost', 'root', '')){
 	if(!$ssql->resetDB('myDB'))
 		$ssql->makeDB('myDB');
-$db = $ssql->selectDB('myDB');
-($db->checkTable('myTable') ? $db->dropTable('myTable') : '');
-$db->makeTable('myTable', ['id', 'firstname', 'lastname', 'email', 'reg_date'], ['int(6)', '', '', 'VARCHAR(50)', 'TIMESTAMP'], ['UNSIGNED', '', '', '', 'CURRENT_TIMESTAMP'], ['AUTO_INCREMENT PRIMARY KEY', 'NOT NULL', 'NOT NULL', '', 'ON UPDATE CURRENT_TIMESTAMP']);
-$db->addData('myTable', ['firstname','lastname','email'],[['John','Doe','john@example.com'],['Fred','Bang','fred@example.com'],['Greg','barns','greg@example.com']]);
-$db->selectData('myTable',['*']);
-$db->dropData('myTable', 'id=3');
-$db->updateData('myTable', 'lastname="Doe"', 'id=2');
-$ssql->close();	
+	$db = $ssql->selectDB('myDB');
+	($db->checkTable('myTable') ? $db->dropTable('myTable') : '');
+	$db->makeTable('myTable', ['id', 'firstname', 'lastname', 'email', 'reg_date'], ['int(6)', '', '', 'VARCHAR(50)', 'TIMESTAMP'], ['UNSIGNED', '', '', '', 'CURRENT_TIMESTAMP'], ['AUTO_INCREMENT PRIMARY KEY', 'NOT NULL', 'NOT NULL', '', 'ON UPDATE CURRENT_TIMESTAMP']);
+	$db->addData('myTable', ['firstname','lastname','email'],[['John','Doe','john@example.com'],['Fred','Bang','fred@example.com'],['Greg','barns','greg@example.com']]);
+	$db->selectData('myTable',['*']);
+	$db->dropData('myTable', 'id=2');
+	$db->updateData('myTable', 'lastname="Doe"', 'id=3');
+	echo $db->makeView('myView','myTable',['firstname','lastname','email']);
+	$db->dropView('myView');
+	$ssql->makeUser('test', $ssql->genPsw('psw'));
+	$ssql->givePerm(['ALL'], '*', ['test']);
+	$ssql->remPerm(['ALL'], '*', ['test']);
+	$ssql->dropUser('test');
+	$ssql->close();	
 }
 ?>
 </body>
--- SSQL/lib/ssql.lib.php
+++ SSQL/lib/ssql.lib.php
@@ -5,12 +5,60 @@ class SSQL{
 	protected $name;
 	protected $psw;
 	protected $conn;
-	
+	protected $db;
+	protected $dm;
 	function __construct(){
 		$this->server='';
 		$this->name='';
 		$this->psw='';
 		$this->conn='';
+		$this->db='';
+		$this->dm = 'dark';
+	}
+	function style($darkmode=true){
+		$this->dm = ($darkmode ? 'dark' : 'light');
+		$out= '<style>
+		.ssql-table{
+			border-collapse: collapse;
+			border-spacing: 0;
+			width: 100%;
+			display: table;
+			border: 1px solid #ccc;
+			margin: 20px 0;
+		}
+		.ssql-table th:first-child, .ssql-table td:first-child {
+			padding-left: 16px;
+		}
+		.ssql-table td, .ssql-table th {
+			padding: 8px 8px;
+			display: table-cell;
+			text-align: left;
+			vertical-align: top;
+		}
+		';
+		if($darkmode){
+			$out.='.ssql-table.dark tr:nth-child(odd) {
+			background-color: #1d2a35;
+			color: #ddd;
+		}
+		.ssql-table.dark tr:nth-child(even) {
+			background-color: #38444d;
+			color: #ddd;
+		}
+		.ssql-table.dark tr {
+			border-bottom: 1px solid #38444d;
+		}';
+		}else{
+			$out.='.ssql-table.light tr:nth-child(odd) {
+			background-color: #ddd;
+			color: #1d2a35;
+			}
+			.ssql-table.light tr {
+				border-bottom: 1px solid #dddddd;
+			}';
+		}
+		$out.='</style>';
+		return $out;
 	}
 	function setCredential(string $s, string $u, string $p) : bool{
 		$this->server = filter_var($s,FILTER_SANITIZE_STRING);
@@ -27,8 +75,10 @@ class SSQL{
 	function close() : bool{
 		return $this->conn->close();
 	}
+	# database
 	function checkDB(string $dbname) : bool{
 		if($this->conn->select_db(strtolower($dbname))){
+			$this->db = strtolower($dbname);
 			return true;
 		}else{
 			return false;
@@ -37,6 +87,7 @@ class SSQL{
 	function dropDB(string $dbname) : bool{
 		$sql = 'DROP DATABASE '.strtolower($dbname);
 		if($this->conn->query($sql)===TRUE){
+			$this->db = '';
 			return true;
 		}else{
 			die('Error: '.$this->conn->error);
@@ -46,6 +97,7 @@ class SSQL{
 	function makeDB(string $dbname) : bool{
 		$sql = 'CREATE DATABASE '.strtolower($dbname);
 		if($this->conn->query($sql)===TRUE){
+			$this->db = strtolower($dbname);
 			return true;
 		}else{
 			die('Error: '.$this->conn->error);
@@ -65,6 +117,7 @@ class SSQL{
 		$this->conn->select_db(strtolower($dbname));
 		return $this;
 	}
+	# tables
 	function makeTable(string $tbname, array $items, array $types ,array $values, array $options) : bool{
 		if(count($items)!=count($types)||count($items)!=count($values)||count($items)!=count($options)){
 			die('All array must match up with items amout(Physical Count: '.count($items).'/Array Count:'.(count($items)-1).')');
@@ -128,8 +181,8 @@ class SSQL{
 			return $row;
 		}
 	}
-	function dropData(string $tbname, string $condition) : bool{
-		$sql = 'DELETE FROM '.strtolower($tbname).' WHERE '.$condition;
+	function dropData(string $tbname, string $condition='') : bool{
+		$sql = 'DELETE FROM '.strtolower($tbname).($condition!=='' ? ' WHERE '.$condition : '');
 		if($this->conn->query($sql)===TRUE){
 			return true;
 		}else{
@@ -137,14 +190,101 @@ class SSQL{
 			return false;
 		}
 	}
-	function updateData(string $tbname, string $replacement, string $condition) : bool{
-		$sql = 'UPDATE '.strtolower($tbname).' SET '.$replacement.' WHERE '.$condition;
+	function updateData(string $tbname, string $replacement, string $condition='') : bool{
+		$sql = 'UPDATE '.strtolower($tbname).' SET '.$replacement.($condition!=='' ? ' WHERE '.$condition : '');
 		if($this->conn->query($sql)===TRUE){
 			return true;
 		}else{
 			die('Error: '.$this->conn->error);
 			return false;
 		}
+	}
+	# permissions
+	function givePerm(array $perm, string $tbname, array $username) : bool{
+		$sql = 'GRANT '.implode(',',$perm).' ON '.$this->db.'.'.strtolower($tbname).' TO '.implode(',',array_map(function($i){return $i.'@'.$this->server;},$username));
+		if($this->conn->query($sql)){
+			return true;
+		}else{
+			die('Error: '.$this->conn->error);
+			return false;
+		}
+	}
+	function remPerm(array $perm,string $tbname, array $username) : bool{
+		$sql = 'REVOKE '.implode(',',$perm).' ON '.$this->db.'.'.strtolower($tbname).' FROM '.implode(',',array_map(function($i){return $i.'@'.$this->server;},$username));
+		if($this->conn->query($sql)){
+			return true;
+		}else{
+			die('Error: '.$this->conn->error);
+				return false;
+		}	
+	}
+	# accounts
+	function makeUser(string $username, string $psw='', bool $checkExists=true ,array $options=[]) : bool{
+		$sql = 'CREATE USER '.($checkExists ? 'IF NOT EXISTS' : '').' "'.$username.'"@"'.$this->server.'" IDENTIFIED BY '.($psw!==''||$psw!==null ? '"'.$psw.'"' : '').' '.implode(' ',$options);
+		if($this->conn->query($sql)){
+			return true;
+		}else{
+			die('Error: '.$this->conn->error);
+				return false;
+		}
+	}
+	function dropUser(string $username){
+		$sql = 'DROP USER "'.$username.'"@"'.$this->server.'"';
+		if($this->conn->query($sql)){
+			return true;
+		}else{
+			die('Error: '.$this->conn->error);
+				return false;
+		}
+	}
+	# views
+	function makeView(string $viewName, string $tbname, array $data, string $condition='', array $options=[]) : string{
+		$sql = 'CREATE OR REPLACE VIEW '.strtolower($viewName).' AS SELECT '.implode(',',$data).' FROM '.$this->db.'.'.strtolower($tbname).($condition!=='' ? 'WHERE '.$condition : '').' '.implode(' ',$options);
+		if($this->conn->query($sql)){
+			$out='<table class="ssql-table '.$this->dm.'">
+			<tbody>';
+			$getView = 'SELECT * FROM '.$viewName;
+			$r = $this->conn->query($getView);
+			$out.='<tr>';
+			foreach($data as $d){
+				$out.='<th>'.$d.'</th>';
+			}
+			$out.='</tr>';
+			while($row = $r->fetch_assoc()){
+				$out.='<tr>';
+				foreach($data as $d){
+					$out.='<td>'.$row[$d].'</td>';
+				}
+				$out.='</tr>';
+			}
+			$out.='</tbody>
+			</table>';
+			return $out;
+		}else{
+			die('Error: '.$this->conn->error);
+				return '';
+		}
+	}
+	function dropView(string $viewName) : bool{
+		$sql = 'DROP VIEW '.strtolower($viewName);
+		if($this->conn->query($sql)){
+			return true;
+		}else{
+			die('Error: '.$this->conn->error);
+				return false;
+		}
+	}
+	
+	# others
+	function genPsw($salt='') {
+		$alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()-_+=';
+		$pass = array(); //remember to declare $pass as an array
+		$alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
+		for ($i = 0; $i < 8; $i++) {
+			$n = rand(0, $alphaLength);
+			$pass[] = $alphabet[$n];
+		}
+		return $salt.implode($pass); //turn the array into a string
 	}
 }
 ?>
